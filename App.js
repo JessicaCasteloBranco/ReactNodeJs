ef7dfd97f6e7c1ee30b30549c070eee77684ad17
Jéssica Castelo Branco <76215697+JessicaCasteloBranco@users.noreply.github.com>
2023-05-22 10:49:54 -0400

    ReactApp

---
+++ App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="JESSICA.png" className="App-logo" alt="logo" />
+        <p>
+          Oie sou o Adriano <span className="heart">♥️</span> gosto de comer bolo
+        </p>
+        <p className="small">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://https://github.com/JessicaCasteloBranco.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+
+
+
+
+
+export default App;
